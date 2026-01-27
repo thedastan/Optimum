916f9e365a45bc8edb8ef841deafa883a383adb0
Alim Mahmudov <alimmahmudov27@gmail.com>
2026-01-27 20:31:09 +0600

    logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,49 +6,50 @@ import { LayoutPage } from "@/components/layout/LayoutPage";
 import Head from "./Head";
 
 const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-	weight: "100 900",
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
 });
 const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
-	weight: "100 900",
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
 export const metadata: Metadata = {
-	title: {
-		absolute: SITE_NAME,
-		template: `%s | ${SITE_NAME}`,
-	},
-	description: SITE_DESCRIPTION,
-	openGraph: {
-		title: SITE_NAME,
-		description: SITE_DESCRIPTION,
-		siteName: SITE_NAME,
-		images: [
-			{
-				url: "/asim.png",
-				alt: SITE_NAME,
-			},
-		],
-		locale: "ru_RU",
-		type: "website",
-	},
+  title: {
+    absolute: SITE_NAME,
+    template: `%s | ${SITE_NAME}`,
+  },
+  description: SITE_DESCRIPTION,
+  openGraph: {
+    title: SITE_NAME,
+    description: SITE_DESCRIPTION,
+    siteName: SITE_NAME,
+    images: [
+      {
+        url: "/image.png",
+        alt: SITE_NAME,
+      },
+    ],
+    locale: "ru_RU",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<Head />
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-				<LayoutPage>{children}</LayoutPage>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <Head />
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <LayoutPage>{children}</LayoutPage>
+      </body>
+    </html>
+  );
 }
--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -1,9 +1,8 @@
-// Имя пректа 
-export const SITE_NAME = 'Asim_project'
+// Имя пректа
+export const SITE_NAME = "Optimum";
 
 // Описание проекта
-export const SITE_DESCRIPTION = 'Asim_project'  
+export const SITE_DESCRIPTION = "Optimum";
 
 // KEY_WORDS проекта
-export const SEO_KEY_WORDS = 'Asim_project'
- 
+export const SEO_KEY_WORDS = "Optimum";
